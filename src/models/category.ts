200bd9b65e4f45ac0c3c830c14578a8d2cbc9c6a
Francisco <franciscojosebellosalcedo@gmail.com>
2023-12-12 18:25:29 -0500

    addeding imagen in the module category

--- src/models/category.ts
+++ src/models/category.ts
@@ -5,6 +5,10 @@ const categorySchema=new mongoose.Schema({
         type:String,
         trim:true
     },
+    imagen:{
+        type:String,
+        trim:true
+    }
 },{timestamps:true});
 
 const Category= mongoose.model('Category',categorySchema);
