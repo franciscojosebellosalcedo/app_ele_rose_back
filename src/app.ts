5d16769dafa1f8e327bb026b64690ac56b525649
Francisco <franciscojosebellosalcedo@gmail.com>
2023-12-26 13:11:27 -0500

    fix request

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import morgan from "morgan";
 
 const app=express();
 
-app.use(express.json());
+app.use(express.json({limit:"200mb"}));
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
