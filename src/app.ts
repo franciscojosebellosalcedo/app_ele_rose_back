8cf1489c86bd381273303951fff0c35b74d880eb
Francisco <franciscojosebellosalcedo@gmail.com>
2024-02-17 12:41:31 -0500

    n

--- src/app.ts
+++ src/app.ts
@@ -8,22 +8,7 @@ config();
 const app=express();
 
 app.use(express.json({limit:"200mb"}));
-
-const allowedOrigins = ['https://page-ele-rose.vercel.app/'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Comprueba si el origen está en la lista de permitidos o si es una solicitud de misma página (null)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET, POST, PUT, DELETE',
-  allowedHeaders: 'Content-Type',
-}));
-
+app.use(cors({origin:"*"}));
 app.use(helmet());
 app.use(morgan("dev"));
 
