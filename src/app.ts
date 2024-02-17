bc51d294bdba0b9cdac30d02db5fc9514d3c6223
Francisco <franciscojosebellosalcedo@gmail.com>
2024-02-17 12:33:11 -0500

    fix

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,12 @@ config();
 const app=express();
 
 app.use(express.json({limit:"200mb"}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://page-ele-rose.vercel.app/');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(cors({origin:["https://page-ele-rose.vercel.app/"]}));
 app.use(helmet());
 app.use(morgan("dev"));
